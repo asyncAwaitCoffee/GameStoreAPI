ab99e092cf81e398fb9f66be2a5c6e71a87c16e3
Andrei Stepanov <steam.oni.am@gmail.com>
2024-03-04 18:26:59 +0400

    +repository

--- Endpoints/GamesEndpoints.cs
+++ Endpoints/GamesEndpoints.cs
@@ -1,69 +1,33 @@
 ﻿using GameStoreApi.Entities;
+using GameStoreApi.Repositories;
 
 namespace GameStoreApi.Endpoints
 {
     public static class GamesEndpoints
     {
-        const string GetGameEndPoint = "GetGame";
-
-        private static List<Game> games = new()
-            {
-                new()
-                {
-                    Id = 1,
-                    Name = "Street Fighter II",
-                    Genre = "Fighting",
-                    Price = 19.99M,
-                    ReleaseDate = new DateTime(1991,2,1),
-                    ImageURI = "https://placehold.co/100"
-                },
-                new()
-                {
-                    Id = 2,
-                    Name = "Final Fantasy XIV",
-                    Genre = "RPG",
-                    Price = 59.99M,
-                    ReleaseDate = new DateTime(2010,9,30),
-                    ImageURI = "https://placehold.co/100"
-                },
-                new()
-                {
-                    Id = 3,
-                    Name = "FIFA 2023",
-                    Genre = "RPG",
-                    Price = 69.99M,
-                    ReleaseDate = new DateTime(2022,9,27),
-                    ImageURI = "https://placehold.co/100"
-                }
-            };
+        const string GetGameEndPoint = "GetGame";        
         public static RouteGroupBuilder MapGamesEndpoints(this IEndpointRouteBuilder routes)
         {
+            InMemGameRepository repository = new InMemGameRepository();
+
             var group = routes.MapGroup("/games").WithParameterValidation();
 
-            group.MapGet("/", () => games);
+            group.MapGet("/", () => repository.GetAll());
             group.MapGet("/{id:int}", (int id) =>
             {
-                Game? game = games.Find(g => g.Id == id);
-
-                if (game == null)
-                {
-                    return Results.NotFound();
-                }
-
-                return Results.Ok(game);
+                Game? game = repository.GetGame(id);
+                return game is not null ? Results.Ok(game) : Results.NotFound();
             }).WithName(GetGameEndPoint);
 
             group.MapPost("/", (Game game) =>
             {
-                game.Id = games.Max(g => g.Id) + 1;
-                games.Add(game);
-
+                repository.CreateGame(game);
                 return Results.CreatedAtRoute(GetGameEndPoint, new { id = game.Id }, game);
             });
 
             group.MapPut("/{id:int}", (int id, Game updatedGame) =>
             {
-                Game? existingGame = games.Find(g => g.Id == id);
+                Game? existingGame = repository.GetGame(id);
 
                 if (existingGame == null)
                 {
@@ -76,16 +40,18 @@ namespace GameStoreApi.Endpoints
                 existingGame.ReleaseDate = updatedGame.ReleaseDate;
                 existingGame.ImageURI = updatedGame.ImageURI;
 
+                repository.UpdateGame(existingGame);
+
                 return Results.NoContent();
             });
 
             group.MapDelete("/{id:int}", (int id) =>
             {
-                Game? existingGame = games.Find(g => g.Id == id);
+                Game? existingGame = repository.GetGame(id);
 
                 if (existingGame != null)
                 {
-                    games.Remove(existingGame);
+                    repository.DeleteGame(id);
                 }
 
                 return Results.NoContent();
---
+++ Repositories/InMemGameRepository.cs
@@ -0,0 +1,66 @@
+﻿using GameStoreApi.Entities;
+
+namespace GameStoreApi.Repositories
+{
+    public class InMemGameRepository
+    {
+        private readonly List<Game> games = new()
+            {
+                new()
+                {
+                    Id = 1,
+                    Name = "Street Fighter II",
+                    Genre = "Fighting",
+                    Price = 19.99M,
+                    ReleaseDate = new DateTime(1991,2,1),
+                    ImageURI = "https://placehold.co/100"
+                },
+                new()
+                {
+                    Id = 2,
+                    Name = "Final Fantasy XIV",
+                    Genre = "RPG",
+                    Price = 59.99M,
+                    ReleaseDate = new DateTime(2010,9,30),
+                    ImageURI = "https://placehold.co/100"
+                },
+                new()
+                {
+                    Id = 3,
+                    Name = "FIFA 2023",
+                    Genre = "RPG",
+                    Price = 69.99M,
+                    ReleaseDate = new DateTime(2022,9,27),
+                    ImageURI = "https://placehold.co/100"
+                }
+            };
+
+        public IEnumerable<Game> GetAll()
+        {
+            return games;
+        }
+
+        public Game? GetGame(int id)
+        {
+            return games.Find(g => g.Id == id);
+        }
+
+        public void CreateGame(Game game)
+        {
+            game.Id = games.Max(g => g.Id) + 1;
+            games.Add(game);
+        }
+
+        public void UpdateGame(Game updatedGame)
+        {
+            var index = games.FindIndex(game => game.Id == updatedGame.Id);
+            games[index] = updatedGame;
+        }
+
+        public void DeleteGame(int id)
+        {
+            var index = games.FindIndex(game => game.Id == id);
+            games.RemoveAt(index);
+        }
+    }
+}
